5d4330057caf5925cca64d1393eb57ad0900db98
divyanshu <divyanshutomar943@gmail.com>
2024-07-06 13:55:48 +0530

    forntend minor changes #4 day

--- config/db.js
+++ config/db.js
@@ -1,19 +1,14 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const  mongoose = require('mongoose')
 
-dotenv.config();
+require('dotenv').config()
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
+            useUnifiedTopology : true
+    }).then(()=>console.log("DB Connected Successfully✅"))
+    .catch((error)=>{ 
+        console.log("this error occured"+ error)
+        process.exit(1)
+    })
+}
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/auth');
-const dotenv = require('dotenv');
+const express = require('express')
 
-dotenv.config();
+const app = express()
 
-const app = express();
-const port = process.env.PORT || 3001;
+require('dotenv').config()
+const PORT = process.env.PORT || 4000
 
-// Connect to MongoDB
-connectDB();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
 
-// Routes
-app.use('/api/auth', authRoutes);
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+//calling Database function
+require('./config/db').connect()
+
+//route importing and mounting
+const user = require('./routes/user')
+
+app.use('/api/v1', user)
+
+
+app.listen(PORT, ()=>{
+    console.log("Server Started")
+   
+})
