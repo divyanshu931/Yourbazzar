b9fb7c61de0dd68cf60183bb06732d57ccbe75ce
divyanshu <divyanshutomar943@gmail.com>
2024-07-12 18:29:38 +0530

    demo deploying updated

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
-const  mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-require('dotenv').config()
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+  
+    
 
-exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology : true
-    }).then(()=>console.log("DB Connected Successfully✅"))
-    .catch((error)=>{ 
-        console.log("this error occured"+ error)
-        process.exit(1)
-    })
-}
+    });
+    console.log('MongoDB Connected ✅');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -33,8 +33,7 @@ app.use(bodyParser.json());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
