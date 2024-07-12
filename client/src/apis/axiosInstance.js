63cbb7e47ea4b69be6462d6c3cd73095525f360e
divyanshu <divyanshutomar943@gmail.com>
2024-07-12 19:06:24 +0530

    Update axiosInstance.js

--- client/src/apis/axiosInstance.js
+++ client/src/apis/axiosInstance.js
@@ -1,7 +1,13 @@
-import axios from "axios";
+// src/apis/axiosInstance.js
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000'; // Fallback to localhost if env variable is not set
 
 const axiosInstance = axios.create({
-  baseURL: "https://yourbazzar-backend.onrender.com", // Base URL for your API
+  baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default axiosInstance;
